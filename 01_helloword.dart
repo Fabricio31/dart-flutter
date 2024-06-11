23a0370b750da1b9867d5f6c6d890165ae9f69b2
FabricioLinux <fabriciobecerra18@gmail.com>
2024-06-11 18:41:03 -0500

    Archivos 1 y 4

---
+++ 01_helloword.dart
@@ -0,0 +1,14 @@
+// Seccion 2 - Video 4 
+void main(){
+    
+    //  var myName = 'Fabricio';
+    //  String myName = 'Fabricio';
+    //  final myName;
+    //  const myName = 'Mundo';
+
+    print('Hola $myName');
+    /*Console: Hola Fabricio*/
+    print('Hola ${ myName.toUpperCase() }'); //Expresion Interpolacion de String
+    /*Console: Hola FABRICIO*/
+
+}
