de5c82cc02613d71c06895c2423fc8e7481f0220
Fabricio31 <fabriciobecerra18@gmail.com>
2024-06-15 17:38:41 -0500

    Añadiendo archivo 08 modificaciones

--- 08_getters_setters.dart
+++ 08_getters_setters.dart
@@ -3,18 +3,29 @@ void main(){
     //Instanciando  = Clase parametro valor
     final mySquare = Square(side: 10);
 
+    mySquare.side = -5;
+
     print('área : ${ mySquare.calculateArea() }');
 
 }
 
 //Clase
 class Square {
-    double side; // side * side 
+    double _side; // side * side 
 
 //Constructor
-    Square({
-        required this.side
-    });
+    Square({ required double side }) 
+    : _side = side;
+
+    double get area {
+        return _side * _side;
+    }
+
+    set side( double value){
+        print('setting new value $value');
+        if (value < 0) throw 'Value must be >= 0';
+        _side = value;
+    }
 
 //0verride idk
     double calculateArea(){
