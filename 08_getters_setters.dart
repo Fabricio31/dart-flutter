c567d1557ac9a98b992415807ef97b75d39fb346
Fabricio31 <fabriciobecerra18@gmail.com>
2024-06-15 17:42:51 -0500

    Añadiendo archivo 08 asserts

--- 08_getters_setters.dart
+++ 08_getters_setters.dart
@@ -15,7 +15,8 @@ class Square {
 
 //Constructor
     Square({ required double side }) 
-    : _side = side;
+    : assert(side >= 0, 'side must be >= 0'),
+     _side = side;
 
     double get area {
         return _side * _side;
@@ -24,6 +25,7 @@ class Square {
     set side( double value){
         print('setting new value $value');
         if (value < 0) throw 'Value must be >= 0';
+
         _side = value;
     }
 
