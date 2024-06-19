3a4b77d94984e48282478130b388dc127dcf7d85
Fabricio31 <fabriciobecerra18@gmail.com>
2024-06-19 16:42:45 -0500

    Añadiendo archivo 10 Mixin

--- 10_mixins.dart
+++ 10_mixins.dart
@@ -1,5 +1,5 @@
 mixin Animal {
-    
+
 }
 
 mixin Mamifero extends Animal {
@@ -12,5 +12,18 @@ mixin Ave extends Animal {
 
 mixin Pez extends Animal {
 
+}
+
+mixin Volador {
+    void volar() => print('estoy volando!');
+}
+
+mixin Caminante {
+    void caminar() => print('estoy caminando!');
+}
+
+
+mixin Nadador {
+    void nadar() => print('estoy nadando !');
 }
 
